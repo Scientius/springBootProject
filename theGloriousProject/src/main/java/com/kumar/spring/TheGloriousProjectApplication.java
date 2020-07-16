8f461ff20a0151741b522f5ce3632091f539adc4
Scientius <45888838+Scientius@users.noreply.github.com>
2020-07-16 16:29:34 -0400

    Add files via upload

---
+++ theGloriousProject/src/main/java/com/kumar/spring/TheGloriousProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kumar.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGloriousProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGloriousProjectApplication.class, args);
+	}
+
+}
---
+++ theGloriousProject/src/test/java/com/kumar/spring/TheGloriousProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGloriousProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
